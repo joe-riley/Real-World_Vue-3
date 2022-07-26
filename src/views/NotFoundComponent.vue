87a8e472f667eebc41c4c6e26316787dadf30639
Joe Riley <rileyjoseph@gmail.com>
2022-07-26 01:35:07 -0700

    Add not found

---
+++ src/views/NotFoundComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>404 - not found</h1>
+</template>
+<script>
+export default {
+  props: {
+    resource: {
+      type: String,
+      required: true,
+      default: 'page'
+    }
+  }
+}
+</script>
